2e269aaa8875ac0c12ebd2ca7539445ae3d6b8a5
Nu36 <nurirt36@uma.es>
2024-01-17 18:50:42 +0100

    fin

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,37 +1,40 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ], callbacks: {
-    async jwt({ token, trigger }) {
-      if (trigger === "signIn") token.signin = "hola";
-      return token;
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ], callbacks: {
+        async jwt({ token, trigger }) {
+            if (trigger === "signIn") token.signin = "hola";
+            return token;
+        },
+        async session({ session, token }) {
+            if (token.signin === "hola") {
+                token.signin = null;
+                const user = session.user;
+                await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/loginlog`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        usuario: user.email,
+                        caducidad: session.expires,
+                        token: token
+                    }),
+                });
+            }
+            return session;
+        },
     },
-    async session({ session, token }) {
-      if (token.signin === "hola") {
-          token.signin = null;
-          const user = session.user;
-          console.log(`El usuario ${user.email} ha iniciado sesión`);
-          await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/loginlog`, {
-              method: "POST",
-              headers: {
-                  "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                  usuario: user.email,
-                  caducidad: session.expires,
-                  token: token
-              }),
-          });
-      }
-      return session;
-    },
-  },
-});
+}
+
+const handler = NextAuth(
+    authOptions
+);
 
 export { handler as GET, handler as POST };
